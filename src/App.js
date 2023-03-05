3bf98c14af6bf6a14e444a07a7e08b24b0f43d0c
Tasnimul Mahi <tasnimul.bin.moshiur@g.bracu.ac.bd>
2023-03-05 10:59:36 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navigation/>
+        < />
+      </div>
+    );
+  }
+}
+
+export default App;
