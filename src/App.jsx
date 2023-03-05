fc1fd7e9786328cde6c95d6d1a2cec4c31a0d59a
TBM-MAHI <tasnimul.bin.moshiur@g.bracu.ac.bd>
2023-03-05 11:23:16 +0600

    preparing folders

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation.jsx';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navigation />
+      {/*   <Logo />
+        <ImageLinkForm/>
+        <FaceRecognition/> */}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Navigation extends Component {
+  render() {
+    return (
+      <nav>
+        <p>Sign out</p>
+      </nav>
+    );
+  }
+}
+
+export default Navigation;
